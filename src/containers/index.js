9fbdcfe206bd1418ea9d12a8a19e84886931e2b5
mhalneami <maan.neami@gmail.com>
2022-02-15 16:38:09 -0500

    first commit

---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export {default as Blog} from './blog/blog';
+export {default as Features} from './features/Features';
+export {default as Footer} from './footer/footer';
+export {default as Header} from './header/Header';
+export {default as Possibility} from './possibility/possibility';
+export {default as WhatGPT3} from './whatGPT3/whatGPT3';
